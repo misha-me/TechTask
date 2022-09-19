86dc63920b8c9788cf101ae78cad56d0b352a006
Serhii Bura <serhiibura@gmail.com>
2022-09-19 23:35:21 +0300

    Review report
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,19 +17,28 @@ public class GameManager : MonoBehaviour
 
         while (!gameOver)
         {
+
+            // Magic Numbers
             foreach (var row in spawner.rows)
             {
                 materialIndex = Random.Range(0, 3);
                 spawner.Spawn(0, 5, materialIndex, row);
             }
+
+            // Wtf???
             if (i % 2 == 0)
             {
                 materialIndex = Random.Range(0, 3);
+                                                  // --- ? What is it? Extract to variable? New?
+                // row = spawner.rows[1] + new Vector3(0, 0, -15);
+                // spawner.Spawn(1, 1, materialIndex, row);
+
                 spawner.Spawn(1, 1, materialIndex, spawner.rows[1] + new Vector3(0, 0, -15));
             }
 
             i++;
 
+            // Why do we need this?
             yield return new WaitForSeconds(5);
         }
     }
